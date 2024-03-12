2b5fdd9a34573d63ee31ddf9ddbc2875a2545e87
Egor.Golikov <Egor.Golikov@cyberprotect.ru>
2024-03-12 21:54:20 +0300

    Increment 5

--- config/flags.go
+++ config/flags.go
@@ -1,6 +1,9 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 var (
 	ServerAdress string
@@ -8,6 +11,13 @@ var (
 )
 
 func ParseFlags() {
+	if envRunAddr := os.Getenv("SERVER_ADDRESS"); envRunAddr != "" {
+		ServerAdress = envRunAddr
+	}
+	if envBaseURL := os.Getenv("BASE_URL"); envBaseURL != "" {
+		BaseURL = envBaseURL
+	}
+
 	flag.StringVar(&ServerAdress, "a", "localhost:8080", "Address and port to run server")
 	flag.StringVar(&BaseURL, "b", "http://localhost:8080", "Base URL for requests")
 	flag.Parse()
